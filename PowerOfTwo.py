11b7eeebbb9f3becee4eb412745b605e74b7b4ab
Doğan Seyfi Şen <doganseyfisen@gmail.com>
2023-07-15 00:10:45 +0300

    Create PowerOfTwo.py

---
+++ PowerOfTwo.py
@@ -0,0 +1,8 @@
+"""
+  @Author: Doğan Seyfi Şen
+"""
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        return n & (n - 1) == 0 # bit conrol
