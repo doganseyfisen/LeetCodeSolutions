7adfc659da6c655c9ad99b99276b4341fd00f2ae
Doğan Seyfi Şen <doganseyfisen@gmail.com>
2023-07-14 20:12:25 +0300

    Create LengthOfLastWord.py

---
+++ LengthOfLastWord.py
@@ -0,0 +1,8 @@
+"""
+  @Author: Doğan Seyfi Şen
+"""
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+        last_word = words[-1]
+        return len(last_word)
