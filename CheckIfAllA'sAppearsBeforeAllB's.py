d3197bb5db0176a6d22d5e4c20116368e2463422
Doğan Seyfi Şen <doganseyfisen@gmail.com>
2023-07-22 22:17:17 +0300

    Create CheckIfAllA'sAppearsBeforeAllB's.py

---
+++ CheckIfAllA'sAppearsBeforeAllB's.py
@@ -0,0 +1,16 @@
+"""
+  @Author:
+"""
+class Solution:
+    def checkString(self, s: str) -> bool:
+        there_is_b_before_a = False
+
+        for char in s:
+            if char == 'b':
+                there_is_b_before_a = True
+            elif char == 'a' and there_is_b_before_a: 
+                # Only T and T gives T, so
+                # When elif's condition is True, next line is going to return False
+                return False
+
+        return True
