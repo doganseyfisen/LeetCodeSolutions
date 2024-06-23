a1033bb3a9383461f2a50554b9ba9fb2c61a0782
Doğan Seyfi Şen <doganseyfisen@gmail.com>
2024-06-23 20:48:22 +0300

    Create UglyNumber.py

---
+++ UglyNumber.py
@@ -0,0 +1,13 @@
+"""
+  @Author: Doğan Seyfi Şen
+"""
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        
+        for prime in [2, 3, 5]:
+            while n % prime == 0:
+                n //= prime
+        
+        return n == 1
